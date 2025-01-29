261959f23b88e13ae171a10c2e195642530c6c18
BrunoPuls <74255789+BrunoPuls@users.noreply.github.com>
2025-01-29 14:57:13 -0300

    Create tabuleiro.h

---
+++ tabuleiro.h
@@ -0,0 +1,28 @@
+#ifndef TABULEIRO_H
+#define TABULEIRO_H
+
+#include <QWidget>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QLabel>
+#include "peca.h" // Inclui a classe Peca corretamente
+
+class Tabuleiro : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit Tabuleiro(QWidget *parent = nullptr);
+
+signals:
+    void buttonClicked(int row, int col); // Sinal emitido quando um botão for clicado
+
+public slots:
+    void marcarBotao(int row, int col); // Slot para marcar um botão correspondente
+
+private:
+    QGridLayout *layout;
+    QPushButton *botoes[10][10]; // Armazena os botões do tabuleiro
+    Peca *peca; // Ponteiro para a peça vinda de peca.h
+};
+
+#endif // TABULEIRO_H
